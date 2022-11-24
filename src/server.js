ef2300bc3e9d49015961709c3c359ad77d053878
leechanggi <lxyex1379@naver.com>
2022-11-24 17:34:05 +0900

    Update server.js

--- src/server.js
+++ src/server.js
@@ -1,17 +1,43 @@
 import express from 'express';
-
+const app = express();
 const PORT = 4000;
 
-const app = express();
-
-const handleHome = (req, res) => {
-  res.send('Hellow Node JS');
+/** MiddleWare 미들웨어 : reqeust 와 response 사이에 존재, express next 매개변수 사용, request 에 응답하지 않음, request를 유지함 */
+const logger = (req, res, next) => {
+  console.log(`${req.method} ${req.url}`);
+  next();
 };
 
-app.get('/', handleHome); // => get request
+const privateMiddleware = (req, res, next) => {
+  const url = req.url;
+  if (url === '/protected') {
+    return res.send('<h1>Not Allowd</h1>');
+  }
+  next();
+};
+
+const handleHome = (req, res) => {
+  res.send('Home');
+};
+
+const handleLogin = (req, res) => {
+  res.send('Login');
+};
+
+const handleProtected = (req, res) => {
+  res.send('Protected');
+};
 
 const handleListening = () => {
   console.log(`Server listenting on post http://localhost:${PORT}`);
 };
+
+/** use() 는 middleware를 모든 get 함수 이전에 적용, use() 와 get()의 위치가 바뀌면 실행이 되지 않는다. */
+app.use(logger, privateMiddleware);
+
+/** routes */
+app.get('/', handleHome);
+app.get('/login', handleLogin);
+app.get('/protected', handleProtected);
 
 app.listen(PORT, handleListening);
