d4c810ec9b5de95cc0467f12fa56db363b9feb81
leechanggi <lxyex1379@naver.com>
2022-11-25 22:39:03 +0900

    add morgan

---
+++ README.md
@@ -0,0 +1,12 @@
+# Wetube Reloaded
+
+/ -> Home
+/join -> Join
+/login -> Login
+/search -> Search
+
+/users/edit -> Edit Profile
+/users/delete -> Delete Profile
+/watch-video -> Watch Video
+/edit-video -> Edit Video
+/delete-video -> Delete Video
--- src/server.js
+++ src/server.js
@@ -1,25 +1,14 @@
 import express from 'express';
+import morgan from 'morgan';
+
 const PORT = 4000;
 const app = express();
 
 /** MiddleWare 미들웨어 : reqeust 와 response 사이에 존재, express next 매개변수 사용, request 에 응답하지 않음, request를 유지함 */
-const logger = (req, res, next) => {
-  console.log(`{method: ${req.method}, url: ${req.url}}`);
-  next();
-};
-const privateMiddleware = (req, res, next) => {
-  const url = req.url;
-  if (url === '/protected') {
-    return res.send('<h1>Not Allowd</h1>');
-  }
-  next();
-};
+const logger = morgan('dev');
 
 const handleHome = (req, res) => {
   res.send('Home');
-
-  /** res.end() : 서버 연결 종료 */
-  // return res.end();
 };
 const handleLogin = (req, res) => {
   res.send('Login');
@@ -33,7 +22,7 @@ const handleListening = () => {
 };
 
 /** use() 는 middleware를 모든 get 함수 이전에 적용, use() 와 get()의 위치가 바뀌면 실행이 되지 않는다. */
-app.use(logger, privateMiddleware);
+app.use(logger);
 
 /** routes */
 app.get('/', handleHome);
