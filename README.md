9489c1b794e5bb70f13f3265e9b2767da4a0ecd7
leechanggi <67743720+leechanggi@users.noreply.github.com>
2022-11-29 11:49:42 +0900

    router

--- README.md
+++ README.md
@@ -5,8 +5,11 @@
 /login -> Login
 /search -> Search
 
-/users/edit -> Edit Profile
-/users/delete -> Delete Profile
-/watch-video -> Watch Video
-/edit-video -> Edit Video
-/delete-video -> Delete Video
+/users/edit -> Edit user
+/users/delete -> Delete user
+
+/videos/watch -> Watch Video
+/videos/edit -> Edit Video
+/videos/delete -> Delete Video
+/videos/comment -> Comment on a Video
+/videos/comment/delete -> Delete A Comment of a Video
--- src/server.js
+++ src/server.js
@@ -3,30 +3,34 @@ import morgan from 'morgan';
 
 const PORT = 4000;
 const app = express();
-
-/** MiddleWare 미들웨어 : reqeust 와 response 사이에 존재, express next 매개변수 사용, request 에 응답하지 않음, request를 유지함 */
 const logger = morgan('dev');
+
+const globalRouter = express.Router();
+const userRouter = express.Router();
+const videosRouter = express.Router();
 
 const handleHome = (req, res) => {
   res.send('Home');
 };
-const handleLogin = (req, res) => {
-  res.send('Login');
+const handleEditUser = (req, res) => {
+  res.send('Edit user');
 };
-const handleProtected = (req, res) => {
-  res.send('Protected');
+const handleWathchVideos = (req, res) => {
+  res.send('Watch Video');
 };
 
 const handleListening = () => {
   console.log(`Server listenting on port http://localhost:${PORT}`);
 };
 
-/** use() 는 middleware를 모든 get 함수 이전에 적용, use() 와 get()의 위치가 바뀌면 실행이 되지 않는다. */
 app.use(logger);
 
-/** routes */
-app.get('/', handleHome);
-app.get('/login', handleLogin);
-app.get('/protected', handleProtected);
+app.use('/', globalRouter);
+app.use('/videos', videosRouter);
+app.use('/user', userRouter);
+
+globalRouter.get('/', handleHome);
+userRouter.get('/edit', handleEditUser);
+videosRouter.get('/watch', handleWathchVideos);
 
 app.listen(PORT, handleListening);
