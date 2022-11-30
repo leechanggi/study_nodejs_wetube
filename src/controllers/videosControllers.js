93a97234f17d386f514bf114c45792644917f209
leechanggi <lxyex1379@naver.com>
2022-11-30 23:10:09 +0900

    modify

--- src/controllers/videosControllers.js
+++ src/controllers/videosControllers.js
@@ -2,14 +2,14 @@ import config from '../config.json';
 const data = { ...config };
 
 let videos = [
-  { title: 'Video #1', rating: 5, comments: 4, createdAt: '2 minutes ago', views: 59, id: 1 },
+  { title: 'Video #1', rating: 5, comments: 4, createdAt: '2 minutes ago', views: 1, id: 1 },
   { title: 'Video #2', rating: 4, comments: 3, createdAt: '3 minutes ago', views: 49, id: 2 },
   { title: 'Video #3', rating: 3, comments: 2, createdAt: '4 minutes ago', views: 39, id: 3 },
 ];
 
 /** global */
 const globalApp = (req, res) => {
-  res.render(data.globalApp.path, Object.assign(data.globalApp, { videos }));
+  res.render(data.globalApp.path, Object.assign({}, data.globalApp, { videos }));
 };
 const globalSearch = (req, res) => {
   res.render(data.globalSearch.path, data.globalSearch);
@@ -19,7 +19,8 @@ const globalSearch = (req, res) => {
 const videoWatch = (req, res) => {
   const { id } = req.params;
   const video = videos[id - 1];
-  res.render(data.videoWatch.path, Object.assign({}, data.videoWatch, { pageTitle: `Watch ${video.title}` }, video));
+  console.log(Object.assign({}, data.videoWatch, { pageTitle: `Watch ${video.title}` }, { video }));
+  res.render(data.videoWatch.path, Object.assign({}, data.videoWatch, { pageTitle: `Watch ${video.title}` }, { video }));
 };
 const videoUpload = (req, res) => {
   res.render(data.videoUpload.path, data.videoUpload);
