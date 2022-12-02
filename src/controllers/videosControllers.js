71ab04627a6e53148a82769477856fce338ef0d7
leechanggi <67743720+leechanggi@users.noreply.github.com>
2022-12-02 16:10:12 +0900

    add The First DATA!

--- src/controllers/videosControllers.js
+++ src/controllers/videosControllers.js
@@ -15,40 +15,50 @@ let data = { ...config };
 const globalHome = async (req, res) => {
   try {
     const videos = await Video.find({});
-    res.render(data.globalHome.renderPath, Object.assign({}, data.globalHome, { videos }));
+    console.log(videos);
+    return res.render(data.globalHome.renderPath, Object.assign({}, data.globalHome, { videos }));
   } catch (error) {
-    res.render('server-error', { error });
+    return res.render('server-error', { error });
   }
 };
 
 const globalSearch = (req, res) => {
-  res.render(data.globalSearch.renderPath, data.globalSearch);
+  return res.render(data.globalSearch.renderPath, data.globalSearch);
 };
 
 /** get - video */
 const videoWatch = (req, res) => {
-  res.render(data.videoWatch.renderPath, Object.assign({}, data.videoWatch, { pageTitle: `Watch ${video.title}` }));
+  return res.render(data.videoWatch.renderPath, Object.assign({}, data.videoWatch, { pageTitle: `Watch ${video.title}` }));
 };
 const videoUpload = (req, res) => {
-  res.render(data.videoUpload.renderPath, data.videoUpload);
+  return res.render(data.videoUpload.renderPath, data.videoUpload);
 };
 const videoEdit = (req, res) => {
-  res.render(data.videoEdit.renderPath, Object.assign({}, data.videoEdit, { pageTitle: `Edit ${video.title}` }));
+  return res.render(data.videoEdit.renderPath, Object.assign({}, data.videoEdit, { pageTitle: `Edit ${video.title}` }));
 };
 const videoRemove = (req, res) => {
-  res.render(data.videoRemove.renderPath, data.videoRemove);
+  return res.render(data.videoRemove.renderPath, data.videoRemove);
 };
 
 /** POST - video */
-const postVideoUpload = (req, res) => {
-  const newVideo = Object.assign({}, req.body, { rating: 0 }, { comments: 0 }, { createdAt: 'just now' }, { views: 0 });
+const postVideoUpload = async (req, res) => {
+  const { title, description, hashtags } = req.body;
+  await Video.create({
+    title,
+    description,
+    createdAt: Date.now(),
+    hashtags: hashtags.split(',').map(word => `#${word}`),
+    meta: {
+      views: 0,
+      rating: 0,
+    },
+  });
   res.redirect('/');
 };
 const postVideoEdit = (req, res) => {
   const { id } = req.params;
   const reqBody = req.body;
   const title = reqBody.title;
-  video.title = title;
   res.redirect(`/videos/${id}`);
 };
 
